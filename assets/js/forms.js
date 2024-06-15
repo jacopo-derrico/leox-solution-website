be705cb4a0e058d169f75f0cb99706794c761ca6
jacopo-derrico <derricoj.design@gmail.com>
2024-06-15 17:35:57 +0200

    feat: add success modal and redirect

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,41 +1,3 @@
-// (() => {
-//     'use strict'
-  
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     const forms = document.querySelectorAll('.needs-validation')
-//     const email = document.getElementById('email');
-  
-//     // Loop over them and prevent submission
-//     Array.from(forms).forEach(form => {
-//       form.addEventListener('submit', event => {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
-//         if(email === '') {
-//             setError(email, 'Email is required');
-//         } else {
-//             setSuccess(email);
-//         }
-  
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-
-//     function setError(input, message) {
-//         const formGroup = input.parentElement;
-//         const feedback = formGroup.querySelector('.invalid-feedback');
-//         feedback.textContent = message;
-//         input.classList.add('is-invalid');
-//         input.classList.remove('is-valid');
-//       }
-
-//       function setSuccess(input) {
-//         input.classList.add('is-valid');
-//         input.classList.remove('is-invalid');
-//       }
-// })()
-
 (() => {
     'use strict'
 
@@ -68,4 +30,10 @@
         form.classList.add('was-validated');
       }, false);
     });
-  })();
+})();
+
+const backHomeBtn = document.getElementById('backToHome');
+
+backHomeBtn.addEventListener('click', function () {
+    window.location.replace("../index.html");
+});
